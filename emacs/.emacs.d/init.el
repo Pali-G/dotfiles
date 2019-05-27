0276d817f1dfbc75da526ab11cd284f0d28ccf35
antivampir <gerbip00@zedat.fu-berlin.de>
2019-05-27 22:50:19 +0200

    initinal commit

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,33 @@
+;; Add repositories
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
+			 ("gnu"   . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")
+			 ("org"   . "https://orgmode.org/elpa/")))
+(package-initialize)
+
+;; Bootstrapping use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
+(let ((config-file (expand-file-name "config.org" user-emacs-directory)))
+  (when (file-readable-p config-file)
+(org-babel-load-file config-file)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (base16-theme which-key use-package treemacs popup-kill-ring nyan-mode magit expand-region counsel company auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
